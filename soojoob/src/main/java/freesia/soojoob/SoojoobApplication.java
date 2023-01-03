d1342ec23b481285b6cc36b2065fe89ffabadd96
sskong777 <sskong777@gmail.com>
2023-01-04 01:06:33 +0900

    spring pjt setting

---
+++ soojoob/src/main/java/freesia/soojoob/SoojoobApplication.java
@@ -0,0 +1,13 @@
+package freesia.soojoob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoojoobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoojoobApplication.class, args);
+	}
+
+}
