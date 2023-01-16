d7f2fdd5e6e200563a4bf0ae24223983a5dc0346
ict.cspark <ict.cspark@gmail.com>
2023-01-16 09:49:01 +0900

    feat:article repository 구현

--- soojoob/src/main/java/freesia/soojoob/article/repository/ArticleRepository.java
+++ soojoob/src/main/java/freesia/soojoob/article/repository/ArticleRepository.java
@@ -1,4 +1,10 @@
 package freesia.soojoob.article.repository;
 
-public interface ArticleRepository {
+import freesia.soojoob.article.entity.Article;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
 }
